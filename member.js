513234f1bc46a7da08bf32de1648c8049d56596c
Dang Tai Bui <44842583+buitai97@users.noreply.github.com>
2024-09-21 05:27:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,77 @@
+function skillsMember(){
+    var member = document.getElementById('member').value;
+    var member = member.toLowerCase();
+    if (member == 'web'){
+        location.href = 'web.html';
+    }
+    else if (member == 'android'){
+        location.href = 'android.html';
+    }
+    else if (member == 'design'){
+        location.href = 'design.html';
+    }
+    else if (member == 'marketing'){
+        location.href = 'marketing.html';
+    }
+    else if (member == 'content'){
+        location.href = 'content.html';
+    }
+    else if (member == 'management'){
+        location.href = 'management.html';
+    }
+    else if (member == 'video'){
+        location.href = 'video.html';
+    }
+    else if (member == 'event'){
+        location.href = 'event.html';
+    }
+    else if (member == 'photography'){
+        location.href = 'photography.html';
+    }
+    else if (member == 'music'){
+        location.href = 'music.html';
+    }
+    else if (member == 'game'){
+        location.href = 'game.html';
+    }
+    else if (member == 'animation'){
+        location.href = 'animation.html';
+    }
+    else if (member == 'writing'){
+        location.href = 'writing.html';
+    }
+    else if (member == 'english'){
+        location.href = 'english.html';
+    }
+    else if (member == 'chinese'){
+        location.href = 'chinese.html';
+    }
+    else if (member == 'japanese'){
+        location.href = 'japanese.html';
+    }
+    else if (member == 'korean'){
+        location.href = 'korean.html';
+    }
+    else if (member == 'french'){
+        location.href = 'french.html';
+    }
+    else if (member == 'spanish'){
+        location.href = 'spanish.html';
+    }
+    else if (member == 'german'){
+        location.href = 'german.html';
+    }
+    else if (member == 'russian'){
+        location.href = 'russian.html';
+    }
+    else if (member == 'dutch'){
+        location.href = 'dutch.html';
+    }
+    else if (member == 'italian'){
+        location.href = 'italian.html';
+    }
+    else if (member == 'portuguese'){
+        location.href = 'portuguese.html';
+    }
+    
+}
